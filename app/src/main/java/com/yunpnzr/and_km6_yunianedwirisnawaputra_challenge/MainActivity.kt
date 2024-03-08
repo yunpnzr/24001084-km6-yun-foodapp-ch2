3cf5dbf075197dba9b37144cf71929952195adcf
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2024-03-09 00:08:10 +0700

    Initial project

---
+++ app/src/main/java/com/yunpnzr/and_km6_yunianedwirisnawaputra_challenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yunpnzr.and_km6_yunianedwirisnawaputra_challenge
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
