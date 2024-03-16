9e80eceaf833f3d652f4dc4333d86d1b2b747554
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2024-03-17 06:10:33 +0700

    Initial commit

---
+++ app/src/main/java/com/yunpnzr/and_km6_yunianedwirisnawaputra_challenge/feature/detail/DetailActivity.kt
@@ -0,0 +1,18 @@
+package com.yunpnzr.and_km6_yunianedwirisnawaputra_challenge.feature.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yunpnzr.and_km6_yunianedwirisnawaputra_challenge.R
+import com.yunpnzr.and_km6_yunianedwirisnawaputra_challenge.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private val binding: ActivityDetailBinding by lazy {
+        ActivityDetailBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
